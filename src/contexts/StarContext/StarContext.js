69c0ed52419f7f94c0e477eb85a08d6074436cf9
GuillemXanxo <jscodebaker@gmail.com>
2022-02-07 18:59:55 +0100

    Changed function from context to provider

--- src/contexts/StarContext/StarContext.js
+++ src/contexts/StarContext/StarContext.js
@@ -2,14 +2,3 @@ const { createContext } = require("react");
 
 const StarContext = createContext();
 export default StarContext;
-
-async function getStarFighters(endpoint) {
-  const firstList = await fetch(endpoint);
-  const arrayStarFighters = await firstList.json();
-  const starFighters = await Promise.all(arrayStarFighters);
-  return starFighters;
-}
-
-const starFightersFetch = await getStarFighters(
-  "https://akabab.github.io/starwars-api/api/all.json"
-);
--- src/contexts/StarContext/StarContextProvider.js
+++ src/contexts/StarContext/StarContextProvider.js
@@ -41,6 +41,17 @@ const StarContextProvider = ({ children }) => {
     formerAffiliations: [],
   };
 
+  async function getStarFighters(endpoint) {
+    const firstList = await fetch(endpoint);
+    const arrayStarFighters = await firstList.json();
+    const starFighters = await Promise.all(arrayStarFighters);
+    return starFighters;
+  }
+
+  const starFightersFetch = await getStarFighters(
+    "https://akabab.github.io/starwars-api/api/all.json"
+  );
+
   return (
     <StarContext.Provider value={{ character }}>
       {children}
