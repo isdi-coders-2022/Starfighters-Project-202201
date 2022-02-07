c158f4d8b5298540ae05a2afb88d580948c503c3
GuillemXanxo <jscodebaker@gmail.com>
2022-02-07 18:54:48 +0100

    Added fetch to remote API

--- src/contexts/StarContext/StarContext.js
+++ src/contexts/StarContext/StarContext.js
@@ -3,3 +3,13 @@ const { createContext } = require("react");
 const StarContext = createContext();
 export default StarContext;
 
+async function getStarFighters(endpoint) {
+  const firstList = await fetch(endpoint);
+  const arrayStarFighters = await firstList.json();
+  const starFighters = await Promise.all(arrayStarFighters);
+  return starFighters;
+}
+
+const starFightersFetch = await getStarFighters(
+  "https://akabab.github.io/starwars-api/api/all.json"
+);
