9778f8ce6d4ce7515b7b71f53d4306dea64f1fe5
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-07 19:50:59 +0100

    Fixed import route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import StarContextProvider from "./contexts/StarContext/StarContextProvider";
+import StarContextProvider from "./store/contexts/StarContext/StarContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
