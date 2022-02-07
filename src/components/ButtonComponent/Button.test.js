d49fcab98bcd706aac4bae6c95a9707dc08f5b78
GuillemXanxo <jscodebaker@gmail.com>
2022-02-07 16:33:39 +0100

    Added button component and started test

---
+++ src/components/ButtonComponent/Button.test.js
@@ -0,0 +1,3 @@
+import Button from "../Button/Button.js";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
--- src/components/ButtonComponent/ButtonComponent.js
+++ src/components/ButtonComponent/ButtonComponent.js
@@ -1,1 +1,13 @@
+import { useContext } from "react";
 
+const Button = (onClickAction, className) => {
+  return (
+    <Button
+      type="button"
+      onClick={onClickAction}
+      className={`${className}`}
+    ></Button>
+  );
+};
+
+export default Button;
