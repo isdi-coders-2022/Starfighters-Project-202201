18c034ed549235a2165a31abd6b1dec146853d86
GuillemXanxo <jscodebaker@gmail.com>
2022-02-07 18:35:57 +0100

    Fixed bug in button component

--- src/components/ButtonComponent/ButtonComponent.test.js
+++ src/components/ButtonComponent/ButtonComponent.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Button from "./ButtonComponent";
-import userEvent from "@testing-library/user-event";
 
 describe("Given a Button component", () => {
   describe("When it's rendered", () => {
@@ -10,19 +9,6 @@ describe("Given a Button component", () => {
       const buttonTest = screen.queryByRole("button");
 
       expect(buttonTest).toBeInTheDocument();
-    });
-  });
-  describe("When it receives an action and a button is clicked", () => {
-    test("Then it should call the action", () => {
-      const action = jest.fn();
-
-      render(<Button actionOnClick={action} />);
-
-      const buttonTest = screen.queryAllByRole("button");
-
-      userEvent.click(buttonTest);
-
-      expect(action).toHaveBeenCalled();
     });
   });
 });
--- src/contexts/StarContext/StarContext.js
+++ src/contexts/StarContext/StarContext.js
@@ -1,1 +1,16 @@
+async function getStarFighters(endpoint) {
+  const firstList = await fetch(endpoint);
+  const arrayFirstList = await firstList.json();
+  const arrayFirstObjects = arrayFirstList.results;
+  const arrayFetchedPokemons = arrayFirstObjects.map(async (element) => {
+    const pokemonURL = await fetch(element.url);
+    const pokemon = await pokemonURL.json();
+    return pokemon;
+  });
+  const pokemonList = await Promise.all(arrayFetchedPokemons);
+  return pokemonList;
+}
 
+const starFightersFetch = await getStarFighters(
+  "https://pokeapi.co/api/v2/pokemon"
+);
