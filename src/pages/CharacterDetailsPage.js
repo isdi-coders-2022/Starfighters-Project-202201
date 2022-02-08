30b43d0139ba164e1fca8c7ecad6e9179b08c897
GuillemXanxo <jscodebaker@gmail.com>
2022-02-08 16:13:25 +0100

    Added .map in characterDetails to render character info

--- src/pages/CharacterDetailsPage.js
+++ src/pages/CharacterDetailsPage.js
@@ -1,5 +1,19 @@
+import { useContext } from "react";
+import StarContext from "../store/contexts/StarContext/StarContext";
+
 const CharacterDetailsPage = () => {
-  return <h2> Returning Character Details Page</h2>;
+  const { starFighters } = useContext(StarContext);
+
+  return (
+    <>
+      <h2> Returning Character Details Page</h2>;
+      <div>
+        {starFighters.map((fighter) => {
+          return <CharacterDetailsPage />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default CharacterDetailsPage;
--- src/pages/CharacterListPage.js
+++ src/pages/CharacterListPage.js
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import StarContext from "../store/contexts/StarContext/StarContext";
+
 const CharacterListPage = () => {
+  const { starFighters } = useContext(StarContext);
   return (
     <>
       <h2> Returning Character List Page</h2>;
       <ul>
-        <CharacterListPage />
+        {starFighters.map((fighter) => {
+          return <CharacterListPage />;
+        })}
       </ul>
     </>
   );
