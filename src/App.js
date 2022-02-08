83d051fec67aef7d2c475b949ef958ec5f57eea5
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-08 09:58:34 +0100

    Revert "Merge branch 'feature/redirects' of https://github.com/isdi-coders-2022/Starfighters-Project-202201 into feature/redirects"
    This reverts commit ed1ca8ae391bb5b282992fea5b7c2491f83c5de2, reversing
    changes made to 9396613c855e0d93b7f26edbc67a166fec2d9fee.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,13 @@ import CharacterCreatorFormPage from "./pages/CharacterCreatorFormPage";
 import CharacterDetailsPage from "./pages/CharacterDetailsPage";
 import CharacterListPage from "./pages/CharacterListPage";
 import HomePage from "./pages/HomePage";
-import styled from "styled-components";
 
-const TitleStar = styled.h1`
-  @font-face {font-family: "Star Jedi"; src: url("//db.onlinewebfonts.com/t/af7c07faaff0d16720c25db7990579e2.eot"); src: url("//db.onlinewebfonts.com/t/af7c07faaff0d16720c25db7990579e2.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/af7c07faaff0d16720c25db7990579e2.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/af7c07faaff0d16720c25db7990579e2.woff") format("woff"), url("//db.onlinewebfonts.com/t/af7c07faaff0d16720c25db7990579e2.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/af7c07faaff0d16720c25db7990579e2.svg#Star Jedi") format("svg"); }
-  }
-  font-family: "Star Jedi";
-  color: yellow;
-  font-size: 70px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 function App() {
   return (
     <>
-      <main className="main row col-12" name="main">
-        <header className="header col-12">
-          <nav className="navigation col-12">
+      <main className="main container" name="main">
+        <header className="header">
+          <nav className="navigation">
             <ul className="navigation__list list-unstyled">
               <li className="navigation__list__matches">icon 1</li>
               <li className="navigation__list__profile">icon 2</li>
@@ -40,6 +29,7 @@ function App() {
               element={<CharacterCreatorFormPage />}
             />
           </Routes>
+
           <TitleStar className="header__logo">
             Star <span className="heading-breaker"></span> Fighters
           </TitleStar>
