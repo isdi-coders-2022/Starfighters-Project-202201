64836b0e2c3192d80ce33910e0eb73a742870b41
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-08 16:46:49 +0100

    Added tests to test folder

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "../App";
 
 describe("Given an App component", () => {
   describe("When it is invoked", () => {
