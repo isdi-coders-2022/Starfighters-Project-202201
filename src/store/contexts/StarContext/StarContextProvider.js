9cb1af063a2d3a8bfe8d3f12799c538bbf85cad1
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-08 13:05:38 +0100

    Fixed fetch function

--- src/store/contexts/StarContext/StarContextProvider.js
+++ src/store/contexts/StarContext/StarContextProvider.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import starFighters from "../../../starFighters";
 import StarContext from "./StarContext";
 
 const StarContextProvider = ({ children }) => {
+  const [fighters, setFighters] = useState([starFighters]);
+
+  useEffect(
+    () => async () => {
+      const fetchedData = await fetch(
+        "https://akabab.github.io/starwars-api/api/all.json"
+      );
+      const array = await fetchedData.json();
+      setFighters([...array]);
+    },
+    [setFighters]
+  );
+
   return (
-    <StarContext.Provider value={{ starFighters }}>
+    <StarContext.Provider value={{ setFighters, fighters }}>
       {children}
     </StarContext.Provider>
   );
