6e787ca2148b3cf378a2247b1bae02bbb1a1c51b
GuillemXanxo <jscodebaker@gmail.com>
2022-02-09 12:57:18 +0100

    Added more images for buttonImage

---
+++ src/components/ButtonImageComponent/ButtonImageComponent.js
@@ -0,0 +1,9 @@
+const ButtonBack = ({ onClickAction }) => {
+  return (
+    <button type="button-back" onClick={onClickAction}>
+      <img src="Back_Button.png" alt="Back" />
+    </button>
+  );
+};
+
+export default ButtonBack;
