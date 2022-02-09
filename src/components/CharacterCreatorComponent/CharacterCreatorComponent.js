dc8653c3f4b4929910129d1d68fb3fe1a21b3515
GuillemXanxo <jscodebaker@gmail.com>
2022-02-09 10:56:01 +0100

    Added structure in creator Page and structure in Form Page

--- src/components/CharacterCreatorComponent/CharacterCreatorComponent.js
+++ src/components/CharacterCreatorComponent/CharacterCreatorComponent.js
@@ -1,1 +1,19 @@
+const CharacterCreatorComponent = ({ type, content, name, inputOnChange }) => {
+  return (
+    <>
+      <div className="mb-3">
+        <label htmlFor={name} className="form-label">
+          {content}
+        </label>
+        <input
+          name={name}
+          onChange={inputOnChange}
+          type={type}
+          className="form-control"
+        />
+      </div>
+    </>
+  );
+};
 
+export default CharacterCreatorComponent;
--- src/pages/CharacterCreatorFormPage.js
+++ src/pages/CharacterCreatorFormPage.js
@@ -1,5 +1,71 @@
+import CharacterCreatorComponent from "../components/CharacterCreatorComponent/CharacterCreatorComponent";
+
 const CharacterCreatorFormPage = () => {
-  return <h2> Returning Character Creator Form Page</h2>;
+  return (
+    <>
+      <h2> Returning Character Creator Form Page</h2>
+      <CharacterCreatorComponent
+        type={"number"}
+        content={"Id"}
+        name={"id"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"text"}
+        content={"Name"}
+        name={"name"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"number"}
+        content={"Height"}
+        name={"height"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"number"}
+        content={"Mass"}
+        name={"mass"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"text"}
+        content={"Gender"}
+        name={"gender"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"text"}
+        content={"Homeworld"}
+        name={"homeworld"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"text"}
+        content={"Species"}
+        name={"species"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"text"}
+        content={"Affiliation"}
+        name={"affiliation"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"text"}
+        content={"Master"}
+        name={"master"}
+        inputOnChange={addData}
+      />
+      <CharacterCreatorComponent
+        type={"url"}
+        content={"Image"}
+        name={"image"}
+        inputOnChange={addData}
+      />
+    </>
+  );
 };
 
 export default CharacterCreatorFormPage;
