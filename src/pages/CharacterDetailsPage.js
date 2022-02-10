00aad406982f96f4381490479c9b3b21ef70369f
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-10 15:14:14 +0100

    Fixed testing ewrror

--- src/pages/CharacterDetailsPage.js
+++ src/pages/CharacterDetailsPage.js
@@ -1,9 +1,7 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import CharactersDetailsComponent from "../components/CharacterDetailsComponent/CharacterDetailsComponent";
-
 import useStarAPI from "../hooks/useStarAPI";
-
 import StarContext from "../store/contexts/StarContext/StarContext";
 
 const CharacterDetailsPage = () => {
