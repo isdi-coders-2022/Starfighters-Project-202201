e3bf4276e80acd686381dca9cb4fd4ab2c39a378
GuillemXanxo <jscodebaker@gmail.com>
2022-02-10 22:54:18 +0100

    Added src to second test button image and corrected the first test

--- src/__tests__/CharacterCreatorFormPage.test.js
+++ src/__tests__/CharacterCreatorFormPage.test.js
@@ -1,6 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import CharacterDetailsPage from "../pages/CharacterDetailsPage";
 import StarContextProvider from "../store/contexts/StarContext/StarContextProvider";
+<<<<<<< Updated upstream
+=======
+import CharacterCreatorFormPage from "../pages/CharacterCreatorFormPage";
+import CreatorFormStyled from "../components/CreatorForm/CreatorForm.style";
+import ButtonImage from "../components/ButtonImageComponent/ButtonImageComponent";
+import userEvent from "@testing-library/user-event";
+>>>>>>> Stashed changes
 
 describe("Given a CharacterCreatorFormPage function", () => {
   describe("When invoked", () => {
@@ -13,6 +20,23 @@ describe("Given a CharacterCreatorFormPage function", () => {
       const element = screen.getByRole("img");
 
       expect(element).toBeInTheDocument();
+    });
+  });
+  describe("When the button is clicked", () => {
+    test("It should call the action", () => {
+      const action = jest.fn();
+      render(
+        <StarContextProvider>
+          <CharacterCreatorFormPage />
+          <CreatorFormStyled>
+            <ButtonImage />
+          </CreatorFormStyled>
+        </StarContextProvider>
+      );
+
+      userEvent.click(screen.queryByRole("button"));
+
+      expect(action).toHaveBeenCalled;
     });
   });
 });
