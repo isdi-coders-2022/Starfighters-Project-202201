1f110d3762beb9698c92608b0d393de91ca1882e
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-10 11:22:47 +0100

    Fixed unused import

--- src/pages/CharacterListPage.js
+++ src/pages/CharacterListPage.js
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import CharactersListComponent from "../components/CharacterListComponent/CharacterListComponent";
-import usePublicAPI from "../hooks/usePublicAPI";
 import StarContext from "../store/contexts/StarContext/StarContext";
 import styled from "styled-components";
 
