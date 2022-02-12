15ba73773636a277859753545aded58d261c4f34
GuillemXanxo <jscodebaker@gmail.com>
2022-02-12 18:02:27 +0100

    Added error component and edited error page

---
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -0,0 +1,12 @@
+const Error = () => {
+  return (
+    <Error>
+      <img
+        alt="Darth Vader says an error occurred."
+        src="https://d2ofqe7l47306o.cloudfront.net/myfreewallpapers/starwars/wallpapers/darth-vader.jpg"
+      />
+    </Error>
+  );
+};
+
+export default Error;
